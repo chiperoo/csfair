27eec713135241b4c1d18d8597581030ea31a471
Oran Chirapuntu <oran.chirapuntu@gmail.com>
2023-04-21 17:27:49 -0400

    Controller actions

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,18 @@
 class HomeController < ApplicationController
+  protect_from_forgery with: :null_session
+
   def index
   end
 
   def submit
+    profession = params[:profession]
+    ProfessionsService.instance.add(profession)
+
+    render plain: "OK"
   end
 
   def word_cloud
+    ProfessionsService.instance.display_cloud
+    send_file 'test.png', type: 'image/png', disposition: 'inline'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   root :to => "home#index"
+
+  post 'profession/submit', to: "home#submit"
+
+  get 'profession/word_cloud', to: "home#word_cloud"
 end
